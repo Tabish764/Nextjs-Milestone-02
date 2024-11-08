476d315aa6f47406f469e791795c27f61286191a
Tabish Raza <ts764raza@gmail.com>
2024-11-09 01:43:56 +0500

    2nd commit

--- app/components/Home/Navbar/MobileNav.tsx
+++ app/components/Home/Navbar/MobileNav.tsx
@@ -13,13 +13,13 @@ const MobileNav = ({closeNav,showNav}:Props) => {
     <>    <div className={`fixed ${navOpen} inset-0 z-30 bg  w-[20%] h-screen `}></div>
       <div className={` ${navOpen} duration-500  text-white fixed justify-center flex flex-col h-full w-[40%] sm:w-[30%] bg-[#000000]  space-y-6 decoration z-[10000]`}>
      
-        {navLinks.map((navLink)=>{
-          return (
-            <Link key={navLink.id} href={navLink.url} >
-              <p className='nav__link sm:text-xl no-underline text-[20px] ml-12 border-b-[1.5px] pb-2 border-white sm:text-[30px]' >{navLink.label}</p>
-            </Link>
-          )
-         })}
+      {navLinks.map((navLink) => (
+  <Link key={navLink?.id} href={navLink?.url || '#'}>
+    <p className='nav__link sm:text-xl no-underline text-[20px] ml-12 border-b-[1.5px] pb-2 border-white sm:text-[30px]'>
+      {navLink?.label || 'Unnamed Link'}
+    </p>
+  </Link>
+))}
         <h2 className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-9 h-9 text-white cursor-pointer' onClick={closeNav} ><IoIosMenu /></h2>
     </div>
     </>
